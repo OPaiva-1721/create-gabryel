b7f0429073cbffe33ac14260151bc77013595aca
Gabryel <gabryelpaiva123@gmail.com>
2026-06-15 23:52:49 -0300

    feat(ddd-template): add health module

--- templates/ddd/src/app.module.ts
+++ templates/ddd/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { ExampleModule } from "./modules/example/example.module";
+import { HealthModule } from "./modules/health/health.module";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ExampleModule,
+    HealthModule,
   ],
 })
 export class AppModule {}
