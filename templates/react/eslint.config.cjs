5dee235eb0427adee164d5198707b8d9004c7fe5
Gabryel <gabryelpaiva123@gmail.com>
2026-06-16 00:14:20 -0300

    feat(react-template): add eslint flat config and prettier

---
+++ templates/react/eslint.config.cjs
@@ -0,0 +1,25 @@
+const tseslint = require("typescript-eslint");
+const reactHooks = require("eslint-plugin-react-hooks");
+const reactRefresh = require("eslint-plugin-react-refresh");
+const eslintConfigPrettier = require("eslint-config-prettier");
+
+module.exports = tseslint.config(
+  {
+    ignores: ["dist", "node_modules"],
+  },
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+  eslintConfigPrettier,
+);
